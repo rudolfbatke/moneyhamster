2745c8a268e1235e991518804d3ec145c3900bbf
Rudolf Batke <rudolf@batke.eu>
2023-09-18 12:05:33 +0100

    feat: add tobnav skeleton
    Signed-off-by: Rudolf Batke <rudolf@batke.eu>

---
+++ src/lib/TopNav.svelte
@@ -0,0 +1,19 @@
+<header>
+	<div>
+		<slot />
+	</div>
+	<div>
+		<slot name="right" />
+	</div>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 1rem;
+		height: 2rem;
+		background-color: #1f1f1f;
+	}
+</style>
---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import './layout.css';
+	import TopNav from '../../lib/TopNav.svelte';
+</script>
+
+<TopNav />
+
+<slot />
