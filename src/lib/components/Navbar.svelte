e117f5f54668efc1062e814c669526a8c1b7e135
Rudolf Batke <rudolf@batke.eu>
2023-09-21 13:03:19 +0100

    feat(categories): add nav item
    Signed-off-by: Rudolf Batke <rudolf@batke.eu>

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,68 @@
+<script>
+	import { browser } from '$app/environment';
+	import SellIcon from '$lib/icons/SellIcon.svelte';
+	import ShoppingCartIcon from '$lib/icons/ShoppingCartIcon.svelte';
+
+	let pathname = '';
+
+	// Get the current pathname
+	if (browser) {
+		pathname = location.pathname;
+	}
+
+	const navItems = [
+		{
+			name: 'Expenses',
+			path: '/app/expenses',
+			icon: ShoppingCartIcon
+		},
+		{
+			name: 'Categories',
+			path: '/app/categories',
+			icon: SellIcon
+		}
+	];
+</script>
+
+<!-- Bottom navigation bar with icons and text -->
+<nav>
+	{#each navItems as item}
+		<a href={item.path} class:active={pathname === item.path}>
+			<svelte:component this={item.icon} />
+			{item.name}
+		</a>
+	{/each}
+</nav>
+
+<style>
+	nav {
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		background-color: var(--background-color);
+		box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.2);
+		z-index: 1;
+		border-top: 1px solid var(--border-color);
+		opacity: 0.8;
+	}
+
+	a {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		text-decoration: none;
+		color: var(--text-color);
+		font-size: 0.8rem;
+		padding: 0.5rem;
+		opacity: 0.8;
+	}
+
+	.active {
+		opacity: 1;
+		--text-color: var(--focus-color);
+	}
+</style>
--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -1,1 +1,14 @@
-<slot />
+<script>
+	import Navbar from '$lib/components/Navbar.svelte';
+</script>
+
+<main>
+	<slot />
+</main>
+<Navbar />
+
+<style>
+	main {
+		padding-bottom: 3.6rem;
+	}
+</style>
