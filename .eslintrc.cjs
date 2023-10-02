13d6d6a36585a074a790877c16320e92e5e7b0e0
Rudolf Batke <rudolf@batke.eu>
2023-10-02 18:31:50 +0100

    chore: repair eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,11 @@
 module.exports = {
 	root: true,
-	extends: ['eslint:recommended', 'prettier', 'plugin:@typescript-eslint/recommended'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	extends: [
+		'eslint:recommended',
+		'prettier',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended'
+	],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020
