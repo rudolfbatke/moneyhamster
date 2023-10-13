49d73284312adb6379e309730688e0e8c9a47acf
Rudolf Batke <rudolf@batke.eu>
2023-10-13 15:00:28 +0100

    chore: rm app sub route
    Signed-off-by: Rudolf Batke <rudolf@batke.eu>

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,77 +1,77 @@
 <script>
-	import SellIcon from '$lib/icons/SellIcon.svelte';
-	import ShoppingCartIcon from '$lib/icons/ShoppingCartIcon.svelte';
-	import { page } from '$app/stores';
-	import MonitoringIcon from '$lib/icons/MonitoringIcon.svelte';
-	import InfoIcon from '$lib/icons/InfoIcon.svelte';
+  import SellIcon from '$lib/icons/SellIcon.svelte';
+  import ShoppingCartIcon from '$lib/icons/ShoppingCartIcon.svelte';
+  import { page } from '$app/stores';
+  import MonitoringIcon from '$lib/icons/MonitoringIcon.svelte';
+  import InfoIcon from '$lib/icons/InfoIcon.svelte';
 
-	let pathname = '';
+  let pathname = '';
 
-	page.subscribe(({ url }) => {
-		pathname = url.pathname;
-	});
+  page.subscribe(({ url }) => {
+    pathname = url.pathname;
+  });
 
-	const navItems = [
-		{
-			name: 'Expenses',
-			path: '/app/expenses',
-			icon: ShoppingCartIcon
-		},
-		{
-			name: 'Categories',
-			path: '/app/categories',
-			icon: SellIcon
-		},
-		{
-			name: 'Charts',
-			path: '/app/charts',
-			icon: MonitoringIcon
-		},
-		{
-			name: 'Info',
-			path: '/app/info',
-			icon: InfoIcon
-		}
-	];
+  const navItems = [
+    {
+      name: 'Expenses',
+      path: '/expenses',
+      icon: ShoppingCartIcon
+    },
+    {
+      name: 'Categories',
+      path: '/categories',
+      icon: SellIcon
+    },
+    {
+      name: 'Charts',
+      path: '/charts',
+      icon: MonitoringIcon
+    },
+    {
+      name: 'Info',
+      path: '/info',
+      icon: InfoIcon
+    }
+  ];
 </script>
 
 <!-- Bottom navigation bar with icons and text -->
 <nav>
-	{#each navItems as item}
-		<a href={item.path} class:active={pathname === item.path}>
-			<svelte:component this={item.icon} />
-			{item.name}
-		</a>
-	{/each}
+  {#each navItems as item}
+    <a href={item.path} class:active={pathname === item.path}>
+      <svelte:component this={item.icon} />
+      {item.name}
+    </a>
+  {/each}
 </nav>
 
 <style>
-	nav {
-		display: flex;
-		justify-content: space-around;
-		align-items: center;
-		background-color: var(--background-color);
-		box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.2);
-		z-index: 10;
-		border-top: 1px solid var(--border-color);
-		height: 3.6rem;
-		width: 100%;
-		border-radius: 1rem 1rem 0 0;
-	}
+  nav {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background-color: var(--background-color);
+    box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.2);
+    z-index: 10;
+    border-top: 1px solid var(--border-color);
+    height: 3.6rem;
+    width: 100%;
+    border-radius: 1rem 1rem 0 0;
+  }
 
-	a {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		text-decoration: none;
-		color: var(--text-color);
-		font-size: 0.8rem;
-		padding: 0.5rem;
-		opacity: 0.7;
-	}
+  a {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-decoration: none;
+    color: var(--text-color);
+    font-size: 0.8rem;
+    padding: 0.5rem;
+    opacity: 0.7;
+  }
 
-	.active {
-		opacity: 1;
-		--text-color: var(--focus-color);
-	}
+  .active {
+    opacity: 1;
+    --text-color: var(--focus-color);
+  }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,15 +4,10 @@
 
 <div>
   <Banner />
-  <p>App in progress...</p>
 </div>
 
 <style>
   div {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
+    padding-top: 9rem;
   }
 </style>
