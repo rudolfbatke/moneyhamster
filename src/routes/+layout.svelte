3eb09e955cafa361a51bfc7a5c296a2c40593220
Rudolf Batke <rudolf@batke.eu>
2023-10-14 10:58:59 +0100

    refactor: replace service worker
    Signed-off-by: Rudolf Batke <rudolf@batke.eu>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,39 +3,11 @@
   import { browser } from '$app/environment';
   import Navbar from '$lib/components/Navbar.svelte';
   import { setAndSupscribeStores } from '$lib/utilities/localStorageStores';
-  import { onMount } from 'svelte';
-  import { pwaInfo } from 'virtual:pwa-info';
 
   if (browser) {
     setAndSupscribeStores();
   }
-
-  $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '';
-
-  onMount(async () => {
-    if (pwaInfo) {
-      const { registerSW } = await import('virtual:pwa-register');
-      registerSW({
-        immediate: true,
-        onRegistered(reg) {
-          // uncomment following code if you want check for updates
-          // r && setInterval(() => {
-          //    console.log('Checking for sw update')
-          //    r.update()
-          // }, 20000 /* 20s for testing purposes */)
-          console.log(`SW Registered: ${reg}`);
-        },
-        onRegisterError(error) {
-          console.log('SW registration error', error);
-        }
-      });
-    }
-  });
 </script>
-
-<svelte:head>
-  {@html webManifestLink}
-</svelte:head>
 
 <div class="container">
   <main>
