8b8fe885922bdab99f19732c1baf4163e5ee1cd3
Rudolf Batke <rudolf@batke.eu>
2023-12-28 12:47:32 +0000

    feat(i18n): add translations

--- src/lib/utilities/translations.js
+++ src/lib/utilities/translations.js
@@ -1,10 +1,50 @@
 const en = {
-  expanses: 'Expanses'
+  categories: 'Categories',
+  category: 'Category',
+  charts: 'Charts',
+  color: 'Color',
+  copy: 'Copy',
+  cost: 'Cost amount',
+  date: 'Date',
+  eg: 'e.g.',
+  expanses: 'Expanses',
+  food: 'Food',
+  issue: 'Issue',
+  apples: 'Äpfel',
+  name: 'Name',
+  newCategory: 'New Category',
+  newExpense: 'New Expense',
+  nowCategory: 'No Category',
+  noItems: 'No Items',
+  pickColor: 'Pick a color',
+  save: 'Save',
+  selectCategory: 'select a category',
+  total: 'Totals'
 };
 
 const de = { ...en };
 
+de.categories = 'Kategorien';
+de.category = 'Kategorie';
+de.charts = 'Graphen';
+de.color = 'Farbe';
+de.copy = 'Kopie';
+de.cost = 'Kosten';
+de.date = 'Datum';
+de.eg = 'z.B.';
 de.expanses = 'Ausgaben';
+de.food = 'Essen';
+de.issue = 'Betreff';
+de.apples = 'Äpfel';
+de.name = 'Name';
+de.newCategory = 'Neue Kategorie';
+de.newExpense = 'Neue Ausgabe';
+de.noItems = 'Keine Einträge';
+de.nowCategory = 'Keine Kategorie';
+de.pickColor = 'Wähle eine Farbe';
+de.save = 'Speichern';
+de.selectCategory = 'wähle eine Kategorie';
+de.total = 'Summe';
 
 export default {
   de,
